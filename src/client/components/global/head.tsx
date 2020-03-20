2482f6a5dfd13625c9dbf6b68efd8e1ab306ab54
David Christian Jones <david@dcj.codes>
2020-03-20 14:46:31 -0400

    tidy

--- src/client/components/global/head.tsx
+++ src/client/components/global/head.tsx
@@ -3,36 +3,18 @@ import { Helmet } from "react-helmet";
 
 const Head = ({
   description = "",
-  locale = "",
-  uri: url = "",
-  root = "",
-  lang = "en",
-  message = "",
+  language = "",
   canonical = "",
   keywords = "",
-  title = `${message}`,
-  ogInfo = {}
+  title = ""
 }) => {
-  let og = {
-    url,
-    locale,
-    ...ogInfo
-  };
-
-  canonical = root + canonical;
-  og = Object.entries(og);
-
   return (
     <Helmet>
-      <html lang={`${lang ? lang : "en"}`} />
+      <html lang={language} />
       <title>{title}</title>
       <meta name="description" content={description} />
       {keywords && <meta name="keywords" content={keywords} />}
       {canonical && <link rel="canonical" href={canonical} />}
-      {og?.length > 0 &&
-        og.map(([key, value]) => (
-          <meta key={key} property={`og:${key}`} content={value} />
-        ))}
     </Helmet>
   );
 };
--- src/client/layouts/default.tsx
+++ src/client/layouts/default.tsx
@@ -1,12 +1,10 @@
 import React from "react";
 
-//* - DEPENDCIES -
-import classNames from "classnames";
-
 //* COMPONENTS
 import RegionSwitch from "../regions/_region-switch";
 
-const DefaultLayout = ({ regions, ...rest }) => RegionSwitch regions={regions} {...rest} />
+const DefaultLayout = ({ regions, ...rest }) => (
+  <RegionSwitch regions={regions} {...rest} />
+);
 
 export default DefaultLayout;
-
