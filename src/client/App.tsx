137b552114801305ce05c8b5002230f3417bef8a
David Christian Jones <david@dcj.codes>
2020-03-20 17:28:39 -0400

    remove dead ref

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,8 +1,5 @@
 import React from "react";
 
-//* - COMPONENTS -
-import ErrorBoundary from "./components/_errors/_boundary";
-
 import Head from "./components/global/head";
 import RouteChange from "./components/global/route-change";
 
