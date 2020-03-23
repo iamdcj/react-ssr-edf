b668902df7e4528e44202d88cc244ccce59fdc6a
David Christian Jones <david@dcj.codes>
2020-03-23 12:56:30 -0400

    test shit

---
+++ jest.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+  setupFilesAfterEnv: ["./enzyme.setup.ts"]
+};
--- src/client/components/presentation/BasicText/BasicText.test.tsx
+++ src/client/components/presentation/BasicText/BasicText.test.tsx
@@ -1,1 +1,24 @@
+import React from "react";
+import { shallow, mount, render } from "enzyme";
 
+import BasicText from ".";
+
+describe("Basic Text", function() {
+  it("should render without throwing an error", function() {
+    expect(
+      shallow(<BasicText />).contains(<div className="foo">Bar</div>)
+    ).toBe(true);
+  });
+
+  it('should be selectable by class "foo"', function() {
+    expect(shallow(<BasicText />).is(".foo")).toBe(true);
+  });
+
+  it("should mount in a full DOM", function() {
+    expect(mount(<BasicText />).find(".foo").length).toBe(1);
+  });
+
+  it("should render to static HTML", function() {
+    expect(render(<BasicText />).text()).toEqual("Bar");
+  });
+});
