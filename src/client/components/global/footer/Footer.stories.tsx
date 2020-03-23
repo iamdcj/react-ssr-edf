fede92a769920a089cda2d70cc01809780af8140
David Christian Jones <david@dcj.codes>
2020-03-23 12:56:23 -0400

    more stories

---
+++ src/client/components/global/footer/Footer.stories.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import Footer from "./Footer";
+
+export default { title: "Site Footer" };
+
+export const Default = () => <Footer />;
---
+++ src/client/components/global/header/Header.stories.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import Header from "./Header";
+
+export default { title: "Site Header" };
+
+export const Default = () => <Header />;
