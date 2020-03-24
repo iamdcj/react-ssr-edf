60c9abf4916955c115247e1b7f36abe44f2df5f0
David Christian Jones <david@dcj.codes>
2020-03-24 12:18:29 -0400

    basic tests for a basic text

--- src/client/components/presentation/BasicText/BasicText.test.tsx
+++ src/client/components/presentation/BasicText/BasicText.test.tsx
@@ -3,7 +3,7 @@ import { mount, render } from "enzyme";
 
 import BasicText from ".";
 
-describe("Basic Text", function() {
+describe("Basic Text - General", function() {
   it("should mount in a full DOM", function() {
     expect(mount(<BasicText />).find("section").length).toBe(1);
   });
@@ -12,5 +12,27 @@ describe("Basic Text", function() {
     expect(render(<BasicText title="Hello World!" />).text()).toEqual(
       "Hello World!"
     );
+  });
+});
+
+describe("Basic Text - Heading", function() {
+  it("with - should contain <h2 /> element", function() {
+    expect(mount(<BasicText title="Hello World!" />).find("h2").length).toBe(1);
+  });
+
+  it("without - should not contain <h2 /> element", function() {
+    expect(mount(<BasicText />).find("h2").length).toBe(0);
+  });
+});
+
+describe("Basic Text - Content", function() {
+  it("with - should contain <span /> element", function() {
+    expect(mount(<BasicText text="Hello World!" />).find("span").length).toBe(
+      1
+    );
+  });
+
+  it("without - should not contain <span /> element", function() {
+    expect(mount(<BasicText />).find("span").length).toBe(0);
   });
 });
