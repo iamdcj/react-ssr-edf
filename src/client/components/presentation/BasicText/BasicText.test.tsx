5a9ca4c52e6bd28c922385bce83fd73ae4a4cbce
David Christian Jones <david@dcj.codes>
2020-03-24 12:13:14 -0400

    complete basic tests example

--- src/client/components/presentation/BasicText/BasicText.test.tsx
+++ src/client/components/presentation/BasicText/BasicText.test.tsx
@@ -1,24 +1,16 @@
 import React from "react";
-import { shallow, mount, render } from "enzyme";
+import { mount, render } from "enzyme";
 
 import BasicText from ".";
 
 describe("Basic Text", function() {
-  it("should render without throwing an error", function() {
-    expect(
-      shallow(<BasicText />).contains(<div className="foo">Bar</div>)
-    ).toBe(true);
-  });
-
-  it('should be selectable by class "foo"', function() {
-    expect(shallow(<BasicText />).is(".foo")).toBe(true);
-  });
-
   it("should mount in a full DOM", function() {
-    expect(mount(<BasicText />).find(".foo").length).toBe(1);
+    expect(mount(<BasicText />).find("section").length).toBe(1);
   });
 
   it("should render to static HTML", function() {
-    expect(render(<BasicText />).text()).toEqual("Bar");
+    expect(render(<BasicText title="Hello World!" />).text()).toEqual(
+      "Hello World!"
+    );
   });
 });
