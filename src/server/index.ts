ee1172650d37df18ab5fb4d363e3b5545dcd3adf
David Christian Jones <david@dcj.codes>
2020-03-24 11:58:56 -0400

    add open to deps

--- src/server/index.ts
+++ src/server/index.ts
@@ -2,6 +2,8 @@ import { app, port } from "./_utilities/serverConfig";
 import { renderApp } from "./rendering/render-app";
 import { renderSitemap } from "./rendering/render-sitemap";
 import returnError from "./errors/returnError";
+import open from "open";
+import { ENV } from "../_constants";
 
 app.get("*/sitemap.xml", (_, res) => {
   try {
@@ -18,6 +20,12 @@ app.get("*", (req, res) => {
     returnError(error, res);
   }
 });
+
+console.log(ENV);
+
+if (ENV === "development") {
+  open("http://localhost:3000");
+}
 
 app.listen(port, () =>
   console.info(`Coming to you live via http://localhost:${port}`)
