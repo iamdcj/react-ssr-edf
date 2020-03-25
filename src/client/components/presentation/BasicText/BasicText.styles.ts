db08681b116f5ca9c82f6c37cac177b274742ef6
David Christian Jones <david@dcj.codes>
2020-03-25 15:34:17 -0400

    use emotion

--- src/client/components/presentation/BasicText/BasicText.styles.ts
+++ src/client/components/presentation/BasicText/BasicText.styles.ts
@@ -1,8 +1,16 @@
-import styled from "styled-components";
+import styled from "@emotion/styled";
 
-const BasicTextContainer = styled.section``;
+const BasicTextContainer = styled.section`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
 
-const BasicTextWrapper = styled.div``;
+const BasicTextWrapper = styled.div`
+  padding: 20px;
+  max-width: 1200px;
+  width: 100%;
+`;
 
 const BasicTextTitle = styled.h2``;
 
