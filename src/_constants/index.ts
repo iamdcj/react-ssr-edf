834a30ea0462df711c266033c22d6abf104d5b89
David Christian Jones <david@dcj.codes>
2020-04-01 13:32:03 -0400

    pain in the fanny

--- src/_constants/index.ts
+++ src/_constants/index.ts
@@ -3,13 +3,13 @@ declare const DEPT_EDF_API_BASE: string;
 declare const DEPT_EDF_PORT: number;
 declare const DEPT_EDF_ENV: string;
 declare const DEPT_EDF_STORE: any;
-declare const IS_BROWSER: boolean;
+declare const isBrowser: boolean;
 
 const ENV = DEPT_EDF_ENV;
 const NODE_ENV = DEPT_EDF_NODE_ENV;
 const API_BASE = DEPT_EDF_API_BASE;
 const PORT = DEPT_EDF_PORT;
 const STORE = DEPT_EDF_STORE;
-const IS_BROWSER = IS_BROWSER;
+const IS_BROWSER = isBrowser;
 
 export { NODE_ENV, ENV, API_BASE, PORT, STORE, IS_BROWSER };
