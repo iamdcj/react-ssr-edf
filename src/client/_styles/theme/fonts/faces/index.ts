56bcc3914ba1276ab5cf9ceea71ca2efcfd18d1c
David Christian Jones <david@dcj.codes>
2020-04-03 18:23:48 -0400

    font theming config

---
+++ src/client/_styles/theme/fonts/faces/index.ts
@@ -0,0 +1,14 @@
+const defaultFont = `
+  @font-face {
+    font-family: 'Inter';
+    font-style:  normal;
+    font-weight: 400;
+    font-display: swap;
+    src: url("/fonts/Inter-Regular.woff") format("woff2"),
+        url("/fonts/Inter-Regular.woff2") format("woff");
+  }
+`;
+
+export default {
+  defaultFont,
+};
--- src/client/_styles/theme/fonts/index.ts
+++ src/client/_styles/theme/fonts/index.ts
@@ -1,19 +1,7 @@
-const defaultFont = `
-  @font-face {
-    font-family: 'Inter';
-    font-style:  normal;
-    font-weight: 400;
-    font-display: swap;
-    src: url("/fonts/Inter-Regular.woff") format("woff2"),
-        url("/fonts/Inter-Regular.woff2") format("woff");
-  }
-`;
+import faces from "./faces";
+import families from "./families";
 
 export const fonts = {
-  fontFace: {
-    defaultFont
-  },
-  fontFamilies: {
-    copy: "Helvetica, sans-serif"
-  }
+  faces,
+  families,
 };
